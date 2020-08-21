35d3fadf7483c04187f0d0127e03d267d518fd06
Abdihakin Elmi <Abdihakiin5452@gmail.com>
2020-08-21 21:11:43 +0530

    modify settings and add more taps

---
+++ Instagram/Views/Cells/HomeFeed/IGFeedPostGeneralTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IGFeedPostGeneralTableViewCell.swift
+//  Instagram
+//
+//  Created by ABDIHAKIN ELMI on 21/08/20.
+//  Copyright © 2020 ABDIHAKIN ELMI. All rights reserved.
+//
+
+import UIKit
+
+class IGFeedPostGeneralTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram/Views/Cells/HomeFeed/IGFeedPostTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  IGFeedPostTableViewCell.swift
+//  Instagram
+//
+//  Created by ABDIHAKIN ELMI on 21/08/20.
+//  Copyright © 2020 ABDIHAKIN ELMI. All rights reserved.
+//
+
+import UIKit
+
+final class IGFeedPostTableViewCell: UITableViewCell {
+
+    static let identifier = "IGFeedPostTableViewCell"
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+   
+    
+}
